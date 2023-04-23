da3acf73e4199f201897438dff4c75755c46801c
ayushmaan <ayushmaanroy.12@gmail.com>
2023-04-23 10:57:14 +0530

    Show Details of meal item completed

--- index.js
+++ index.js
@@ -19,21 +19,13 @@ searchBar.addEventListener("keyup", (e) => {
     //if this key's pressed don't search
     return;
   }
-  //   mealList.innerHTML = "";
   let searchValue = e.target.value.trim();
-  //   let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-  //   fetchMealsList(url, value);
+
   fetchMealApiResults(searchValue);
 });
 
-searchBtn.addEventListener("click", (e) => {
-  //   console.log(e);
-
-  if (e.key == " " || e.key == "Tab") {
-    //if this key's pressed don't search
-    return;
-  }
-  let searchValue = e.target.value;
+searchBtn.addEventListener("click", () => {
+  let searchValue = searchBar.value.trim();
   fetchMealApiResults(searchValue);
 });
 
@@ -71,12 +63,12 @@ let displayMealList = (meals) => {
         } else {
           html += `
             <div id="card" class="card mb-4 bg-dark shadow bg-body-tertiary rounded" style="width: 20rem;">
-                <img src="${item.strMealThumb}" class="card-img-top" alt="...">
+                <img src="${item.strMealThumb}" class="card-img-top" alt="..." >
                 <div class="card-body">
                     <h5 class="card-title">${item.strMeal}</h5>
                     <div class="d-flex justify-content-between mt-5">
                         <button type="button" class="btn btn-warning" onclick="showMealDetails(${item.idMeal})">Recipe</button>
-                        <button id="main${item.idMeal}" class="btn btn-outline-danger" ><i class="fa-regular fa-bookmark fa-lg" style="color: #221f51;"></i></button>
+                        <button id="main${item.idMeal}" class="btn btn-outline-danger"   onclick="addRemoveToFavList(${item.idMeal})" ><i class="fa-regular fa-bookmark fa-lg" style="color: #221f51;"></i></button>
                     </div>
                 </div>
             </div>
@@ -120,3 +112,41 @@ let fetchMealApiResults = async (searchValue) => {
     console.error(err);
   }
 };
+async function showMealDetails(id) {
+  let html = "";
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+  const data = await response.json();
+
+  if (data) {
+    html += ` <div class="container py-3">
+    <div class="card  p-lg-5 p-md-2">
+        <div class="row ">
+            <div class="col-md-4  align-self-center" id= "photo">
+                <img src="${data.meals[0].strMealThumb}" class="w-100 ">
+            </div>
+            <div class="col-md-8 px-3 align-self-center">
+                <div class="card-block px-3">
+                   
+                    <h2 class="card-title" id="heading">${data.meals[0].strMeal}</h2>
+                    <p id="category">Category : ${data.meals[0].strCategory}</p>
+                    <p d="area">Area : ${data.meals[0].strArea}</p>
+
+
+                    <h5>Instruction :</h5>
+                    <p class="card-text" id="instructions">
+                        ${data.meals[0].strInstructions}</p>
+                    <a href="${data.meals[0].strYoutube}"  target="_blank" class="btn btn-warning">Video</a>
+
+                    
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>`;
+  }
+
+  mealList.innerHTML = html;
+}
